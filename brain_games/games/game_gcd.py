94203208287f7b3ba6fe0e17e244f8f5c020ca98
Yulia Savanova <sav.jul@gmail.com>
2021-05-10 00:28:30 +0300

    Add new gcd_game

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from random import randint
+
+GAME_RULES = "Find the greatest common divisor of given numbers."
+
+NUMBERS_LEFT_BOUNDARY = 1
+NUMBERS_RIGHT_BOUNDARY = 100
+
+
+def generate_game_data():
+    first_number = randint(NUMBERS_LEFT_BOUNDARY, NUMBERS_RIGHT_BOUNDARY)
+    second_number = randint(NUMBERS_LEFT_BOUNDARY, NUMBERS_RIGHT_BOUNDARY)
+
+    first_number_origin = first_number
+    second_number_origin = second_number
+
+    while first_number != 0 and second_number != 0:
+        if first_number > second_number:
+            first_number = first_number % second_number
+        else:
+            second_number = second_number % first_number
+
+    answer = first_number + second_number
+
+    return f"{first_number_origin} {second_number_origin}", answer
